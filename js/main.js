947309c01c44089882db2c5a7b13e40e02acbd40
Schwupsie <hallo@philipbellmann.de>
2026-05-09 15:00:31 +0200

    ups falscher commit ich versteh das noch nicht so ganz

--- js/main.js
+++ js/main.js
@@ -447,8 +447,8 @@ async function checkMotionPermission() {
 			const permission = await DeviceMotionEvent.requestPermission();
 
 			if (permission === "granted") {
-				// btn_reqPermission.textContent = `Schummelzähler: ${schummelzaehler}`;
-				// btn_reqPermission.style.textDecoration = "none";
+				btn_reqPermission.textContent = `Schummelzähler: ${schummelzaehler}`;
+				btn_reqPermission.style.textDecoration = "none";
 				closeInfo();
 				setMotionListeners();
 			}
@@ -456,8 +456,8 @@ async function checkMotionPermission() {
 			console.log("Permission error:", e);
 		}
 	} else {
-		// btn_reqPermission.textContent = `Schummelzähler: ${schummelzaehler}`;
-		// btn_reqPermission.style.textDecoration = "none";
+		btn_reqPermission.textContent = `Schummelzähler: ${schummelzaehler}`;
+		btn_reqPermission.style.textDecoration = "none";
 		closeInfo();
 		setMotionListeners();
 	}
@@ -480,6 +480,7 @@ function setMotionListeners() {
 
 		if (max > 10) {
 			schummelzaehler++;
+			display.textContent = "Schummeln aktiviert!";
 		}
 	});
 }

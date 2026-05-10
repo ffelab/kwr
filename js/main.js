b0ba696c67c3a3a769bc632716352df8e52d3a4b
Schwupsie <hallo@philipbellmann.de>
2026-05-10 19:40:27 +0200

    threshold 80

--- js/main.js
+++ js/main.js
@@ -497,7 +497,7 @@ function setMotionListeners() {
 	let isShaking = false;
 	let lastShakeTime = 0;
 
-	const SHAKE_THRESHOLD = 40;
+	const SHAKE_THRESHOLD = 80;
 	const RESET_THRESHOLD = 2;
 	const COOLDOWN = 1000;
 

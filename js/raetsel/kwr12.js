7a8b53dd9162445d29e34b28b34e205e9b33cac7
Schwupsie <hallo@philipbellmann.de>
2026-06-23 19:06:56 +0200

    new words

---
+++ js/raetsel/kwr12.js
@@ -0,0 +1,101 @@
+const puzzleData = {
+	PUZZLE_ID: "12",
+	SIZE: 11,
+	MIN_WORD_LENGTH: 3,
+	BLACK_FIELDS: [
+		[0, 10],
+
+		[1, 1],
+		[1, 3],
+		[1, 5],
+		[1, 7],
+		[1, 9],
+
+		[2, 5],
+
+		[3, 1],
+		[3, 3],
+		[3, 5],
+		[3, 7],
+		[3, 9],
+
+		[4, 0],
+		[4, 6],
+
+		[5, 1],
+		[5, 3],
+		[5, 5],
+		[5, 7],
+		[5, 9],
+
+		[6, 4],
+		[6, 10],
+
+		[7, 1],
+		[7, 3],
+		[7, 5],
+		[7, 7],
+		[7, 9],
+
+		[8, 5],
+
+		[9, 1],
+		[9, 3],
+		[9, 5],
+		[9, 7],
+		[9, 9],
+
+		[10, 0],
+	],
+
+	CLUES: {
+		WAAGERECHT: {
+			1: {
+				c: "Bloß nicht noch so einen abonnieren",
+				l: "10",
+				s: "NEWSLETTER",
+			},
+			7: { c: "Wohnort von Oskar", l: "5", s: "TONNE" },
+			8: { c: "Die Hinweise sind aus meiner", l: "5", s: "FEDER" },
+			9: { c: "Bestes Kartenspiel zu deutsch", l: "5", s: "BESEN" },
+			10: {
+				c: "Wenn die Software noch nicht ganz fertig ist",
+				l: "4",
+				s: "BETA",
+			},
+			13: { c: "Was willst du mit dem Pastis, ya!", l: "4", s: "RAKI" },
+			14: {
+				c: "'Fix it in post' für Grafik Designer*innen",
+				l: "5",
+				s: "REPRO",
+			},
+			17: { c: "Tanz in die fünfte Jahreszeit", l: "5", s: "GARDE" },
+			18: { c: "Massiv am Cruisen damals", l: "5", s: "TOENE" },
+			19: {
+				c: "Vorm Tag am See – checkt ihr?",
+				l: "10",
+				s: "REGENRADAR",
+			},
+		},
+		SENKRECHT: {
+			1: {
+				c: "Bekomme hoffentlich die beste hierfür",
+				l: "4",
+				s: "NOTE",
+			},
+			2: { c: "Führt hoch und runter", l: "11", s: "WANDERKARTE" },
+			3: { c: "Hannes hat davon mindesdtens 7", l: "6", s: "LIEDER" },
+			4: { c: "Sicher ohne Rücksicht auf Verluste", l: "4", s: "TIFF" },
+			5: {
+				c: "Von woaus Elia dieses Semester postet",
+				l: "11",
+				s: "ERDBEERFELD",
+			},
+			6: { c: "Gehört zum error wie zum student", l: "5", s: "TRIAL" },
+			11: { c: "Hat der Hydrogeologe intus", l: "5", s: "DROGE" },
+			12: { c: "So eine scharfe Grafik", l: "6", s: "VEKTOR" },
+			15: { c: "Virales Internetphänomen", l: "4", s: "MEME" },
+			16: { c: "Taste zwischen drin", l: "4", s: "LEER" },
+		},
+	},
+};
--- js/script.js
+++ js/script.js
@@ -89,36 +89,52 @@ const numberFields = [
 
 const clues = {
 	WAAGERECHT: {
-		1: { c: "", l: "10", s: "NEWSLETTER" },
-
-		7: { c: "", l: "5", s: "TONNE" },
-		8: { c: "", l: "5", s: "FEDER" },
-
-		9: { c: "", l: "5", s: "BESEN" },
-		10: { c: "", l: "4", s: "BETA" },
-
-		13: { c: "", l: "4", s: "RAKI" },
-		14: { c: "", l: "5", s: "REPRO" },
-
-		17: { c: "", l: "5", s: "GARDE" },
-		18: { c: "", l: "5", s: "TOENE" },
-
-		19: { c: "", l: "10", s: "REGENRADAR" },
+		1: {
+			c: "Bloß nicht noch so einen abonnieren",
+			l: "10",
+			s: "NEWSLETTER",
+		},
+		7: { c: "Wohnort von Oskar", l: "5", s: "TONNE" },
+		8: { c: "Die Hinweise sind aus meiner", l: "5", s: "FEDER" },
+		9: { c: "Bestes Kartenspiel zu deutsch", l: "5", s: "BESEN" },
+		10: {
+			c: "Wenn die Software noch nicht ganz fertig ist",
+			l: "4",
+			s: "BETA",
+		},
+		13: { c: "Was willst du mit dem Pastis, ya!", l: "4", s: "RAKI" },
+		14: {
+			c: "'Fix it in post' für Grafik Designer*innen",
+			l: "5",
+			s: "REPRO",
+		},
+		17: { c: "Tanz in die fünfte Jahreszeit", l: "5", s: "GARDE" },
+		18: { c: "Massiv am Cruisen damals", l: "5", s: "TOENE" },
+		19: {
+			c: "Vorm Tag am See – checkt ihr?",
+			l: "10",
+			s: "REGENRADAR",
+		},
 	},
 	SENKRECHT: {
-		1: { c: "", l: "4", s: "NOTE" },
-		2: { c: "", l: "11", s: "WANDERKARTE" },
-		3: { c: "", l: "6", s: "LIEDER" },
-		4: { c: "", l: "4", s: "TIFF" },
-		5: { c: "", l: "11", s: "ERDBEERFELD" },
-
-		6: { c: "", l: "5", s: "TRIAL" },
-
-		11: { c: "", l: "5", s: "DROGE" },
-		12: { c: "", l: "6", s: "VEKTOR" },
-
-		15: { c: "", l: "4", s: "MEME" },
-		16: { c: "", l: "4", s: "LEER" },
+		1: {
+			c: "Bekomme hoffentlich die beste hierfür",
+			l: "4",
+			s: "NOTE",
+		},
+		2: { c: "Führt hoch und runter", l: "11", s: "WANDERKARTE" },
+		3: { c: "Hannes hat davon mindesdtens 7", l: "6", s: "LIEDER" },
+		4: { c: "Sicher ohne Rücksicht auf Verluste", l: "4", s: "TIFF" },
+		5: {
+			c: "Von woaus Elia dieses Semester postet",
+			l: "11",
+			s: "ERDBEERFELD",
+		},
+		6: { c: "Gehört zum error wie zum student", l: "5", s: "TRIAL" },
+		11: { c: "Hat der Hydrogeologe intus", l: "5", s: "DROGE" },
+		12: { c: "So eine scharfe Grafik", l: "6", s: "VEKTOR" },
+		15: { c: "Virales Internetphänomen", l: "4", s: "MEME" },
+		16: { c: "Taste zwischen drin", l: "4", s: "LEER" },
 	},
 };
 
